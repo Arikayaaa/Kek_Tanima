0c3485e4c1d43a3e53e5d2f9721a25217d4361cc
Süleyman Arıkaya <suleymanarikaya32@gmail.com>
2022-05-20 22:10:19 +0300

    v1.00
    Project documents were added

---
+++ dasar_haartrain/02 haarTraining.bat
@@ -0,0 +1,3 @@
+haartraining.exe -data cascades -vec vector/vector.vec -bg negative/bg.txt -npos 78 -nneg 345 -nstages 25 -mem 4096 -mode ALL -w 24 -h 24 
+
+rem -nonsym
---
+++ dasar_haartrain0/02 haarTraining.bat
@@ -0,0 +1,3 @@
+haartraining.exe -data cascades -vec vector/vector.vec -bg negative/bg.txt -npos 293 -nneg 395 -nstages 15 -mem 4096 -mode ALL -w 24 -h 24 
+
+rem -nonsym
